6f7c99de86c3fb608f52bff88ca7efd9c7c8cb61
Julieta Pailler <jepailler@gmail.com>
2022-07-06 08:52:05 -0300

    funcionamiento formulario

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer')
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', );
-});
+}) 
+
+router.post('/',async function(req,res,next){
+// console.log(req.body)
+
+var nombre = req.body.nombre;
+var email = req.body.email;
+var tel = req.body.tel;
+var comentarios = req.body.comentarios;
+// console.log(req.body.nombre)
+
+var obj = {
+    to:'jepailler@gmail.com',
+    subject:'Contacto desde la página web',
+    html: nombre + " se contactó a través de la web y quiere más información a este correo: " + email + "<br> Su teléfono es: " + tel + ". Su comentario es: " + comentarios + "."//cuerpo del mensaje que va a llegar de aviso
+}
+
+var transport = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+  }
+}); //finaliza el transport
+
+var info = await transport.sendMail(obj); //mismo nombre que la variable creada anteriormente "transport"
+
+res.render('',{
+  message: 'Su mensaje ha sido enviado correctamente'
+})
+
+}); //finaliza router.post
+
 
 module.exports = router;
