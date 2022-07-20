258059ba2c7b242747471e79d885e55ce5597da1
Julieta Pailler <jepailler@gmail.com>
2022-07-19 22:34:42 -0300

    funcionamiento del login

---
+++ models/usuariosModel.js
@@ -0,0 +1,19 @@
+var pool = require ('./bd');
+var md5 = require ('md5');
+
+async function getUserAndPassword(user, password){
+    try{
+        var query = 'select * from usuarios where usuario = ? and password = ? limit 1';
+        var rows = await pool.query(query,[user, md5(password)]);
+        return rows[0];//reforzar la query, que traiga solo un usuario creado con el mismo nombre (limit 1). Rows[0] es para que traiga el primero que reciba con el nombre indicado, Porque el 'select' siempre trae un array de elementos
+
+    }catch(error){
+        console.log(error);
+    }
+} 
+
+module.exports = {getUserAndPassword}
+
+//try-catch: tirar por consola los datos que no entienda especificando el error
+// se usa corchete xq es un array de elementos
+//los datos que estan dentro de parentesis luego del nombre de la función, son parámetros  (informacion que necesitamos pasarle a la función)
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var usuariosModel = require('./../../models/usuariosModel');
 
 
 router.get('/', function (req, res, next) {
@@ -9,6 +9,31 @@ router.get('/', function (req, res, next) {
 
   });
 }) //view/admin/login.hbs
+
+//capturar los datos mediante el método post
+router.post('/', async function(req,res,next){
+
+    try{
+        console.log(req.body);
+        var usuario = req.body.usuario; //captura el dato usuario
+        var password = req.body.password; //captura el dato contraseña
+        //una vez capturados los datos hay que conectar con el archivo que conecta con la base de datos que es el usuariosModel: 
+
+        var data = await usuariosModel.getUserAndPassword(usuario,password);
+
+        if( data != undefined){
+          res.redirect('/admin/promociones')
+        }else{
+          res.render('admin/login',{
+            layout:'admin/layout',
+            error:true
+          })
+        }
+
+    }catch(error){
+      console.log(error)
+    }
+});
 
 
 module.exports = router;
