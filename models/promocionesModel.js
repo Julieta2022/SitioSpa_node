b524da618a84a0bd316a4d98a78d60ce5eb59595
Julieta Pailler <jepailler@gmail.com>
2022-07-24 19:58:15 -0300

    responsive parte 2

--- models/promocionesModel.js
+++ models/promocionesModel.js
@@ -1,51 +1,51 @@
 var pool = require('./bd');
 
-async function getPromociones(){
+async function getPromociones() {
     var query = 'select * from promociones';
-    var rows= await pool.query(query);
+    var rows = await pool.query(query);
     return rows;
 }// limit 4 es para que muestre solo 4
 
-async function getPromocionesfront(){
+async function getPromocionesfront() {
     var query = 'select * from promociones limit 4';
-    var rows= await pool.query(query);
+    var rows = await pool.query(query);
     return rows;
 }// limit 4 es para que muestre solo 4 en el front
 
-
-async function insertPromociones(obj){
-    try{
+async function insertPromociones(obj) {
+    try {
         var query = 'insert into promociones set ?';
-        var rows = await pool.query(query,[obj]);
+        var rows = await pool.query(query, [obj]);
         return rows;
 
-    }catch(error){
+    } catch (error) {
         console.log(error);
         throw error; //el error va por un camino o por otro, en el caso q no este cargado algún campo sale un error o en el caso q no se cargue el combo porq se cae el servidor
     }
 }
 
-async function deletePromocionByID(id){
+async function deletePromocionByID(id) {
     var query = 'delete from promociones where id= ?'
-    var rows = await pool.query(query,[id]);
+    var rows = await pool.query(query, [id]);
     return rows;
 }
 
-async function getPromocionesByID(id){ var query = 'select * from promociones where id = ?'
-var rows = await pool.query(query, [id]);
-return rows [0];//si o si trae una sola, xq es un array
+async function getPromocionesByID(id) {
+    var query = 'select * from promociones where id = ?'
+    var rows = await pool.query(query, [id]);
+    return rows[0];//si o si trae una sola, xq es un array
 }
 
-async function updatePromocionByID(obj,id){
-    try{
-    var query = 'update promociones set ? where id=?'
-    var rows = await pool.query(query,[obj,id]);
-    return rows;
+async function updatePromocionByID(obj, id) {
+    try {
+        var query = 'update promociones set ? where id=?'
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
 
-    }catch (error){
+    } catch (error) {
         // console.log(error)
         throw error;
-    }    
+    }
 }
 
 module.exports = { getPromociones, insertPromociones, deletePromocionByID, getPromocionesByID, updatePromocionByID, getPromocionesfront }
--- routes/index.js
+++ routes/index.js
@@ -3,47 +3,50 @@ var router = express.Router();
 var nodemailer = require('nodemailer')
 var promocionesModel = require('../models/promocionesModel');
 
+
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   var promociones = await promocionesModel.getPromocionesfront();
 
-  res.render('index',{
+  res.render('index', {
     promociones
   });
-}) 
-
-
-router.post('/',async function(req,res,next){
-// console.log(req.body)
-
-var nombre = req.body.nombre;
-var email = req.body.email;
-var tel = req.body.tel;
-var comentarios = req.body.comentarios;
-// console.log(req.body.nombre)
-
-var obj = {
-    to:'jepailler@gmail.com',
-    subject:'Contacto desde la página web',
-    html: nombre + " se contactó a través de la web y quiere más información a este correo: " + email + "<br> Su teléfono es: " + tel + ". Su comentario es: " + comentarios + "."//cuerpo del mensaje que va a llegar de aviso
-}
-
-var transport = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS
-  }
-}); //finaliza el transport
-
-var info = await transport.sendMail(obj); //mismo nombre que la variable creada anteriormente "transport"
-
-res.render('',{
-  message: 'Su mensaje ha sido enviado correctamente'
 })
+
+
+router.post('/', async function (req, res, next) {
+  // console.log(req.body)
+
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var comentarios = req.body.comentarios;
+  // console.log(req.body.nombre)
+
+  var obj = {
+    to: 'jepailler@gmail.com',
+    subject: 'Contacto desde la página web',
+    html: nombre + " se contactó a través de la web y quiere más información a este correo: " + email + "<br> Su teléfono es: " + tel + ". Su comentario es: " + comentarios + "."//cuerpo del mensaje que va a llegar de aviso
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  }); //finaliza el transport
+
+  var info = await transport.sendMail(obj); //mismo nombre que la variable creada anteriormente "transport"
+
+  res.render('', {
+    message: 'Su mensaje ha sido enviado correctamente'
+  })
 
 }); //finaliza router.post
 
 
 module.exports = router;
+
+
